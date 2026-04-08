4ea3ebac485e5233f02a74439a44df595e8c93ce
johann <wenzheng.fang@rivai.ai>
2026-04-08 07:42:08 +0000

    refactor: drop weak from legacy hook declarations

--- extensions/integration/sparta_legacy_hook_abi.h
+++ extensions/integration/sparta_legacy_hook_abi.h
@@ -4,12 +4,12 @@
 #include <cstdint>
 #include <memory>
 
-__attribute__((weak)) void decodeHook(void* fetch, uint64_t pc, uint64_t npc);
-__attribute__((weak)) bool commitHook();
-__attribute__((weak)) uint64_t getNpcHook(uint64_t npc);
-__attribute__((weak)) reg_t excptionHook(void* fetch, uint64_t pc, trap_t& trap);
-__attribute__((weak)) void catchDataBeforeWriteHook(uint64_t addr, uint64_t data, uint32_t len, std::shared_ptr<bool> real_store);
-__attribute__((weak)) void catchDataBeforeCsrHook(int which, uint64_t value, std::shared_ptr<bool> real_store);
-__attribute__((weak)) bool getCsrHook(int which, uint64_t value);
-__attribute__((weak)) bool continueHook();
-__attribute__((weak)) bool exitHook(int code);
+void decodeHook(void* fetch, uint64_t pc, uint64_t npc);
+bool commitHook();
+uint64_t getNpcHook(uint64_t npc);
+reg_t excptionHook(void* fetch, uint64_t pc, trap_t& trap);
+void catchDataBeforeWriteHook(uint64_t addr, uint64_t data, uint32_t len, std::shared_ptr<bool> real_store);
+void catchDataBeforeCsrHook(int which, uint64_t value, std::shared_ptr<bool> real_store);
+bool getCsrHook(int which, uint64_t value);
+bool continueHook();
+bool exitHook(int code);
