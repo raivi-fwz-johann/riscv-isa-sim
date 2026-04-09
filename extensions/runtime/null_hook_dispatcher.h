ae4d5cdabe92332dace3f8cd176bb19ea98bbef6
johann <wenzheng.fang@rivai.ai>
2026-04-09 01:36:26 +0000

    refactor: drop redundant null hook dispatcher

--- extensions/runtime/null_hook_dispatcher.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "runtime/spike_hook_dispatcher.h"
-
-class null_hook_dispatcher_t final : public spike_hook_dispatcher_t {
-public:
-  ~null_hook_dispatcher_t() override = default;
-};
