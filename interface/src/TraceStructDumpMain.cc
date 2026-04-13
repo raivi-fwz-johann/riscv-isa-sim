2acb45c678887474d48ee13504d00e2df4ef531c
johann <wenzheng.fang@rivai.ai>
2026-04-13 08:45:21 +0000

    test: expand trace struct array dump output

--- interface/src/TraceStructDumpMain.cc
+++ interface/src/TraceStructDumpMain.cc
@@ -5,6 +5,51 @@
 
 namespace {
 
+void dump_ptw_steps(const std::vector<PTWStep>& steps)
+{
+  std::cout << "[";
+  for (size_t i = 0; i < steps.size(); ++i) {
+    if (i) {
+      std::cout << ",";
+    }
+    std::cout << "{idx:" << std::dec << i
+              << ",paddr:0x" << std::hex << steps[i].paddr
+              << ",pte:0x" << steps[i].pte << "}";
+  }
+  std::cout << "]";
+}
+
+void dump_regs(const std::vector<RegValue>& regs)
+{
+  std::cout << "[";
+  for (size_t i = 0; i < regs.size(); ++i) {
+    if (i) {
+      std::cout << ",";
+    }
+    std::cout << "{idx:" << std::dec << i
+              << ",flat_id:0x" << std::hex << regs[i].flat_id << "}";
+  }
+  std::cout << "]";
+}
+
+void dump_mem_ops(const std::vector<MemOp>& mem_ops)
+{
+  std::cout << "[";
+  for (size_t i = 0; i < mem_ops.size(); ++i) {
+    if (i) {
+      std::cout << ",";
+    }
+    std::cout << "{idx:" << std::dec << i
+              << ",vaddr:0x" << std::hex << mem_ops[i].vaddr
+              << ",paddr:0x" << mem_ops[i].paddr
+              << ",size_bytes:" << std::dec << unsigned(mem_ops[i].size_bytes)
+              << ",ptw_steps:";
+    dump_ptw_steps(mem_ops[i].ptw_steps);
+    std::cout << "}";
+  }
+  std::cout << "]";
+}
+
 void dump_inst(const InstTrace& inst)
 {
   const auto& trace = inst.newTrace;
@@ -22,7 +67,17 @@ void dump_inst(const InstTrace& inst)
             << " vtype=0x" << std::hex << trace.vtype
             << " vl=" << std::dec << trace.vl
             << " vstart=" << trace.vstart
-            << " active_mask=0x" << std::hex << trace.active_mask
+            << " active_mask=0x" << std::hex << trace.active_mask;
+
+  std::cout << " fetch_ptw_steps=";
+  dump_ptw_steps(trace.fetch_ptw);
+  std::cout << " src_reg_list=";
+  dump_regs(trace.src_regs);
+  std::cout << " dst_reg_list=";
+  dump_regs(trace.dst_regs);
+  std::cout << " mem_op_list=";
+  dump_mem_ops(trace.mem_ops);
+  std::cout
             << std::endl;
 }
 
